24b253f4931e7889fd9e7b140b95383fb26a21f3
zhangkaka <kakapang@foxmail.com>
2016-08-26 17:20:00 +0800

    Graph

---
+++ showmecode/graph.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+
+class EdgeNode(object):
+    def __init__(self):
+        self.adjvex = None
+        self.next = None
+
+class VertexNode(object):
+    def __init__(self,data = "#"):
+        self.data = data
+        self.firstedge = None
+    def __str__(self):
+        return "Instance of VertexNode %s,%s" % (self.data,self.firstedge)
+
+class Graph(object):
+    def __init__(self,vnum,enum):
+        self.numVertexes = vnum
+        self.numEdges = enum
+        self.adjlist = []
+        for i in range(1,100):
+            self.adjlist.append(VertexNode())
+
+        for i,v in enumerate(["v0","v1","v2","v3"]):
+            self.adjlist[i].data = v
+            self.adjlist[i].firstedge = None
+
+        for e in range(0,5):
+            print ("please input the edge number of (vi,vj)")
+            p = input()
+            q = input()
+            m = self._locate(self,p)
+            n = self._locate(self,q)
+
+            edgei = EdgeNode()
+            edgei.adjvex = n
+            edgei.next = self.adjlist[m].firstedge
+            self.adjlist[m].firstedge = edgei
+
+            edgej = EdgeNode()
+            edgej.adjvex = m
+            edgej.next = self.adjlist[n].firstedge
+            self.adjlist[n].firstedge = edgej
+
+    def _locate(self,g,ch):
+        for i in range(0,len(g.adjlist)):
+            if ch == g.adjlist[i].data: return i
+        return -1
+
+    def printGraph(self,g):
+        print "print Graph"
+        i = 0
+        while (i <= 100 and g.adjlist[i].firstedge):
+            print g.adjlist[i].data
+            e = g.adjlist[i].firstedge
+            while e:
+                print e.adjvex
+                e = e.next
+            i += 1
+            print
+
+g1 = Graph(4,6)
+
+g1.printGraph(g1)
